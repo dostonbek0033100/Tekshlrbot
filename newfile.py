01b0383afd85099da9ec20631456ed18c82fe075
dostonbek <74980968+dostonbek0033100@users.noreply.github.com>
2026-09-10 08:31:02 +0500

    Change TOKEN environment variable to BOT_TOKEN
    Updated environment variable for token retrieval.

--- newfile.py
+++ newfile.py
@@ -15,7 +15,7 @@ from telegram.ext import (
 # SOZLAMALAR
 # ============================================================
 
-TOKEN = os.getenv("8766627088:AAHJAxw6qM9jy_O2T1pudmobV1dG4CFD398")
+TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
     raise RuntimeError(
